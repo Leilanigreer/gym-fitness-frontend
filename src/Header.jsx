59013b5a11c4b22bc36022a9da0a14772f1a1654
Leilanigreer <leilani@lpcgolf.com>
2024-11-07 10:47:24 -0800

    added current user name to display and created logic for signup/login or logout

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,41 @@
+import { Link } from "react-router-dom";
+import { LogoutLink } from "./LogoutLink";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export function Header() {
+  const [currentUser, setCurrentUser] = useState ({});
+
+  const getUserData = () => {
+    axios.get("http://localhost:3000/users/current.json").then(response => {
+      console.log(response.data)
+      setCurrentUser(response.data)
+    })
+  };
+
+  useEffect(getUserData, []);
+
+  let authenticationLinks;
+
+  if (localStorage.jwt === undefined) {
+    authenticationLinks = (
+      <>
+      <Link to="/Signup">Signup</Link> | 
+      <Link to="/Login">Login</Link>
+      </>
+    )
+  } else {
+    authenticationLinks = (
+      <LogoutLink />
+    )
+  };
+
   return (
     <header>
       <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
+        <a href="/">Home</a> | {authenticationLinks}
       </nav>
+        <h2>Hi {currentUser.name}!</h2>
     </header>
   )
 }
