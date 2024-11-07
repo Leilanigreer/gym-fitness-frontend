5bbcbf09fdfb3805aa3cc0c1bb0a9766ceff35ab
Leilanigreer <leilani@lpcgolf.com>
2024-11-07 10:48:37 -0800

    changed verbiage

--- src/PlaceholderPage.jsx
+++ src/PlaceholderPage.jsx
@@ -3,7 +3,7 @@ import { ExercisesIndex } from "./ExercisesIndex";
 export function PlaceholderPage() {
   return (
     <main>
-      <h1>Welcome to React, Here we go!</h1>
+      <h1>Are you ready to get in shape? Here we go!</h1>
       <ExercisesIndex />
     </main>
   )
