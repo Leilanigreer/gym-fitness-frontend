e037e788fb19ff7a7972c2459acd7079ca592cfc
Leilanigreer <leilani@lpcgolf.com>
2024-11-06 18:16:03 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { PlaceholderPage } from "./PlaceholderPage";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <PlaceholderPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/PlaceholderPage.jsx
@@ -0,0 +1,7 @@
+export function PlaceholderPage() {
+  return (
+    <main>
+      <h1>Welcome to React, Here we go!</h1>
+    </main>
+  )
+}
